7c0272e8ac9e7ab746d8fc635d36a3e543e9c50a
r9nsubroza <artsoucor12@gmail.com>
2022-09-11 15:47:20 -0300

    completed seeds

--- backend/src/database/seeds/insert_usuario.ts
+++ backend/src/database/seeds/insert_usuario.ts
@@ -7,16 +7,28 @@ const seedUtils = new SeedUtils();
 export async function seed(knex: Knex): Promise<void> {
   // Deletes ALL existing entries
   await knex.raw(`
-    DELETE FROM video;
-    DELETE FROM canal;
-    DELETE FROM usuario;
+  DELETE FROM ad_has_categoria;
+  DELETE FROM video_has_categoria;
+  DELETE FROM playlist_has_video;
+  DELETE FROM playlist;
+  DELETE FROM comentario;
+  DELETE FROM likes;
+  DELETE FROM assistir_mais_tarde;
+  DELETE FROM historico_has_video;
+  DELETE FROM historico;
+  DELETE FROM post;
+  DELETE FROM ad;
+  DELETE FROM video;
+  DELETE FROM canal;
+  DELETE FROM usuario;
+  DELETE FROM categoria;
     `);
 
   // Inserts seed entries
   const userIdsCreated = await seedUtils.insertData(
     knex,
     'usuario',
-    seedUtils.createRandomUsers(5),
+    seedUtils.createRandomUsers(100),
   );
 
   const channelIdsCreated = await seedUtils.insertData(
@@ -28,7 +40,86 @@ export async function seed(knex: Knex): Promise<void> {
   const videoIdsCreated = await seedUtils.insertData(
     knex,
     'video',
-    seedUtils.createRandomVideos(1, channelIdsCreated),
+    seedUtils.createRandomVideos(100, channelIdsCreated),
+  );
+  const postIdsCreated = await seedUtils.insertData(
+    knex,
+    'post',
+    seedUtils.createRandomPost(5, channelIdsCreated),
+  );
+  await seedUtils.insertData(
+    knex,
+    'comentario',
+    seedUtils.createRandomComment(5, userIdsCreated, videoIdsCreated),
+    false,
+  );
+  await seedUtils.insertData(
+    knex,
+    'likes',
+    seedUtils.createRandomLike(5, userIdsCreated, videoIdsCreated),
+    false,
+  );
+  await seedUtils.insertData(
+    knex,
+    'assistir_mais_tarde',
+    seedUtils.createRandomWatchLater(5, userIdsCreated, videoIdsCreated),
+    false,
+  );
+  const historyIdsCreated = await seedUtils.insertData(
+    knex,
+    'historico',
+    seedUtils.createRandomHistory(userIdsCreated.length, userIdsCreated),
+  );
+  await seedUtils.insertData(
+    knex,
+    'historico_has_video',
+    seedUtils.createRandomHistoryHasVideo(
+      5,
+      videoIdsCreated,
+      historyIdsCreated,
+    ),
+    false,
+  );
+  const playlistIdsCreated = await seedUtils.insertData(
+    knex,
+    'playlist',
+    seedUtils.createRandomPlaylist(5, userIdsCreated),
+  );
+  await seedUtils.insertData(
+    knex,
+    'playlist_has_video',
+    seedUtils.createRandomPlaylistHasVideo(
+      5,
+      playlistIdsCreated,
+      videoIdsCreated,
+    ),
+    false,
+  );
+  const categoriaIdsCreated = await seedUtils.insertData(
+    knex,
+    'categoria',
+    seedUtils.createRandomCategoria(5),
+  );
+  await seedUtils.insertData(
+    knex,
+    'video_has_categoria',
+    seedUtils.createRandomVideoHasCategoria(
+      5,
+      categoriaIdsCreated,
+      videoIdsCreated,
+    ),
+    false,
+  );
+  const adIdsCreated = await seedUtils.insertData(
+    knex,
+    'ad',
+    seedUtils.createRandomAd(5),
+  );
+  await seedUtils.insertData(
+    knex,
+    'ad_has_categoria',
+    seedUtils.createRandomAdHasCategoria(5, categoriaIdsCreated, adIdsCreated),
+    false,
   );
 
   // const res = await knex.raw(`select * from video;`);
--- backend/src/database/utils/index.ts
+++ backend/src/database/utils/index.ts
@@ -6,10 +6,22 @@ import { Knex } from 'nestjs-knex';
 export default class SeedUtils {
   randomArrayElement = (array) =>
     array[Math.floor(Math.random() * array.length)];
+  //tentando resolver conflito de chaves
+  // combineArrays(arr1: any[], arr2: any[]) {
+  //     const [lessLength, moreLength] =
+  //       arr1.length < arr2.length ? [arr1, arr2] : [arr2, arr1];
+
+  //     return lessLength.map((e) => {
+  //       const index = Math.floor(Math.random() * moreLength.length);
+  //       const element = moreLength[index];
+  //       moreLength.splice(index, 1);
+  //       return [e, element];
+  //     });
+  //   }
 
   createRandomUsers(n: number) {
     return Array.from({ length: n }).map(() => ({
-      nome: faker.name.fullName(),
+      nome: faker.name.firstName(),
       email: faker.internet.email(),
       data_de_criacao: faker.date.past().toISOString(),
     }));
@@ -38,11 +50,103 @@ export default class SeedUtils {
       canal_id: this.randomArrayElement(canal_ids),
     }));
   }
+  createRandomPost(n: number, canal_ids: number[]) {
+    return Array.from({ length: n }).map(() => ({
+      texto: faker.lorem.sentence(),
+      canal_id: this.randomArrayElement(canal_ids),
+    }));
+  }
+  createRandomComment(n: number, usuario_ids: number[], video_ids: number[]) {
+    return Array.from({ length: n }).map(() => ({
+      texto: faker.lorem.sentence(),
+      video_id: this.randomArrayElement(video_ids),
+      usuario_id: this.randomArrayElement(usuario_ids),
+    }));
+  }
+  createRandomLike(n: number, usuario_ids: number[], video_ids: number[]) {
+    return Array.from({ length: n }).map(() => ({
+      video_id: this.randomArrayElement(video_ids),
+      usuario_id: this.randomArrayElement(usuario_ids),
+    }));
+  }
+  createRandomWatchLater(
+    n: number,
+    usuario_ids: number[],
+    video_ids: number[],
+  ) {
+    return Array.from({ length: n }).map(() => ({
+      video_id: this.randomArrayElement(video_ids),
+      usuario_id: this.randomArrayElement(usuario_ids),
+    }));
+  }
+  createRandomHistory(n: number, usuario_ids: number[]) {
+    return Array.from({ length: n }).map((e, i) => ({
+      usuario_id: usuario_ids[i],
+    }));
+  }
+  createRandomHistoryHasVideo(
+    n: number,
+    video_ids: number[],
+    historico_ids: number[],
+  ) {
+    return Array.from({ length: n }).map((e, i) => ({
+      data_e_hora: faker.date.past().toISOString(),
+      video_id: this.randomArrayElement(video_ids),
+      historico_id: this.randomArrayElement(historico_ids),
+    }));
+  }
+  createRandomPlaylist(n: number, usuario_ids: number[]) {
+    return Array.from({ length: n }).map((e, i) => ({
+      nome: faker.music.genre(),
+      usuario_id: this.randomArrayElement(usuario_ids),
+    }));
+  }
+  createRandomPlaylistHasVideo(
+    n: number,
+    playlist_ids: number[],
+    video_ids: number[],
+  ) {
+    return Array.from({ length: n }).map((e, i) => ({
+      video_id: this.randomArrayElement(video_ids),
+      playlist_id: this.randomArrayElement(playlist_ids),
+    }));
+  }
+  createRandomCategoria(n: number) {
+    return Array.from({ length: n }).map((e, i) => ({
+      descricao: faker.music.genre(),
+    }));
+  }
+  createRandomVideoHasCategoria(
+    n: number,
+    categoria_ids: number[],
+    video_ids: number[],
+  ) {
+    return Array.from({ length: n }).map((e, i) => ({
+      video_id: this.randomArrayElement(video_ids),
+      categoria_id: this.randomArrayElement(categoria_ids),
+    }));
+  }
+  createRandomAd(n: number) {
+    return Array.from({ length: n }).map(() => ({
+      duracao: faker.random.numeric(3),
+    }));
+  }
+  createRandomAdHasCategoria(
+    n: number,
+    categoria_ids: number[],
+    ad_ids: number[],
+  ) {
+    return Array.from({ length: n }).map((e, i) => ({
+      ad_id: this.randomArrayElement(ad_ids),
+      categoria_id: this.randomArrayElement(categoria_ids),
+    }));
+  }
 
   async insertData(
     knex: Knex,
     table: string,
     datas: { [key: string]: any }[],
+    has_id: boolean = true,
   ): Promise<number[]> {
     const keys = Object.keys(datas[0]);
 
@@ -51,11 +155,12 @@ export default class SeedUtils {
       .map((data) => '(' + keys.map((key) => `'${data[key]}'`).join(',') + ')')
       .join(',');
 
-    const queryResponse = await knex.raw(`
+    const queryResponse = await knex.raw(
+      `
       INSERT INTO ${table} (${keys})
       VALUES ${dataFormatted}
-      RETURNING id;
-    `);
+    ` + (has_id ? `RETURNING id;` : `;`),
+    );
 
     return queryResponse.rows.map((data) => data.id);
   }
