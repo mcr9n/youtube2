be1d1849d5b21d978c4be95104a9298a6f9eb3c8
Marcelo <marcelopianopatuscosantiago@gmail.com>
2022-09-12 15:05:21 -0300

    feat: query videos

--- backend/src/video/dto/create-video.dto.ts
+++ backend/src/video/dto/create-video.dto.ts
@@ -1,1 +1,7 @@
-export class CreateVideoDto {}
+export class CreateVideoDto {
+  titulo: number;
+  duracao: string;
+  qualidade: number;
+  data_de_criacao: Date;
+  thumbnail: string;
+}
--- backend/src/video/video.service.ts
+++ backend/src/video/video.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectKnex, Knex } from 'nestjs-knex';
+import { CRUD } from '../utils/crud';
+
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
 
 @Injectable()
 export class VideoService {
+  CRUD: CRUD;
+
+  constructor(@InjectKnex() private readonly knex: Knex) {
+    this.knex = knex;
+    this.CRUD = new CRUD(this.knex);
+  }
+
   create(createVideoDto: CreateVideoDto) {
-    return 'This action adds a new video';
+    return this.CRUD.create('video', [createVideoDto]);
   }
 
   findAll() {
-    return `This action returns all video`;
+    return this.CRUD.read('video', ' order by data_de_criacao DESC limit 50');
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} video`;
+    return this.CRUD.read('video', `video.id = ${id}`);
   }
 
   update(id: number, updateVideoDto: UpdateVideoDto) {
-    return `This action updates a #${id} video`;
+    return this.CRUD.update('youtube', updateVideoDto, `video.id == ${id}`);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} video`;
+    return this.CRUD.delete('youtube', `video.id == ${id} `);
   }
 }
